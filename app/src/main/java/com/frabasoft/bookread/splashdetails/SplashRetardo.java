48b18d7027f7787516724dff5e4c625c1890f070
FrancoBalsamo <70456335+FrancoBalsamo@users.noreply.github.com>
2022-06-10 12:10:52 -0300

    Primer commit, inicio del proyecto

---
+++ app/src/main/java/com/frabasoft/bookread/splashdetails/SplashRetardo.java
@@ -0,0 +1,12 @@
+package com.frabasoft.bookread.splashdetails;
+
+import android.app.Application;
+import android.os.SystemClock;
+
+public class SplashRetardo extends Application {
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        SystemClock.sleep(4000);
+    }
+}
